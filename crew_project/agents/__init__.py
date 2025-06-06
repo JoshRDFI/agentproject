9033544ed0f2bacd89b6f421643d40cb3acdc1f8
JoshRDFI <you@example.comjrogers@rdfindustries.com>
2025-06-06 16:09:32 -0700

    Remove crew_project folder from Git tracking but keep it locally

--- crew_project/agents/__init__.py
+++
@@ -1,4 +0,0 @@
-# Import agents here for easy access
-from .base_agents import ResearchAgent, AnalysisAgent, WriterAgent, WebSearchAgent, ManagerAgent, PDFProcessingAgent
-
-__all__ = ['ResearchAgent', 'AnalysisAgent', 'WriterAgent', 'WebSearchAgent', 'ManagerAgent', 'PDFProcessingAgent']
--- crew_project/tasks/__init__.py
+++
@@ -1,4 +0,0 @@
-# Import tasks here for easy access
-from .base_tasks import ResearchTask, AnalysisTask, WritingTask, WebSearchTask, ManagerTask, PDFProcessingTask
-
-__all__ = ['ResearchTask', 'AnalysisTask', 'WritingTask', 'WebSearchTask', 'ManagerTask', 'PDFProcessingTask']
